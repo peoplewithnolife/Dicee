ea422d4f8332081e7b82fb3b81ea2ca576fd9287
Phil <phils453@gmail.com>
2019-12-24 14:47:56 -0500

    done mostly

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-function dorkNine {
+var randomNumber1 = Math.floor((Math.random()*6)) + 1;
+var randomNumber2 = Math.floor((Math.random()*6)) + 1;
+var diceImages = ["dice1.png","dice2.png","dice3.png","dice4.png","dice5.png","dice6.png"];
+
+//alert("images/"+diceImages[randomNumber1-1]);
+
+document.querySelector("img.img1").setAttribute("src","images/"+diceImages[randomNumber1-1]);
+document.querySelector("img.img2").setAttribute("src","images/"+diceImages[randomNumber2-1]);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").textContent="Player 1 Wins";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").textContent="Player 2 Wins";
+}
+else {
+  document.querySelector("h1").textContent="Tie";
+}
+
+function dorkNine() {
    alert("phil suxarino");
 }
